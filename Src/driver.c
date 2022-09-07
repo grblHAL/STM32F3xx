6fde1c34d0bbb7421aeff8d8ae6b1132479fd1b2
Terje Io <terjeio@users.noreply.github.com>
2022-09-07 21:14:25 +0200

    Updated for grbl/limits.h name change and HAL version number increase.

--- Src/driver.c
+++ Src/driver.c
@@ -29,7 +29,7 @@
 #include "driver.h"
 #include "serial.h"
 
-#include "grbl/limits.h"
+#include "grbl/machine_limits.h"
 #include "grbl/motor_pins.h"
 #include "grbl/pin_bits_masks.h"
 #include "grbl/state_machine.h"
@@ -871,7 +871,7 @@ static char *port2char (GPIO_TypeDef *port)
     return name;
 }
 
-static void enumeratePins (bool low_level, pin_info_ptr pin_info)
+static void enumeratePins (bool low_level, pin_info_ptr pin_info, void *data)
 {
     static xbar_t pin = {0};
     uint32_t i = sizeof(inputpin) / sizeof(input_signal_t);
@@ -886,7 +886,7 @@ static void enumeratePins (bool low_level, pin_info_ptr pin_info)
         pin.description = inputpin[i].description;
         pin.mode.pwm = pin.group == PinGroup_SpindlePWM;
 
-        pin_info(&pin);
+        pin_info(&pin, data);
     };
 
     pin.mode.mask = 0;
@@ -899,7 +899,7 @@ static void enumeratePins (bool low_level, pin_info_ptr pin_info)
         pin.port = low_level ? (void *)outputpin[i].port : (void *)port2char(outputpin[i].port);
         pin.description = outputpin[i].description;
 
-        pin_info(&pin);
+        pin_info(&pin, data);
     };
 
     periph_signal_t *ppin = periph_pins;
@@ -912,7 +912,7 @@ static void enumeratePins (bool low_level, pin_info_ptr pin_info)
         pin.mode = ppin->pin.mode;
         pin.description = ppin->pin.description;
 
-        pin_info(&pin);
+        pin_info(&pin, data);
     } while((ppin = ppin->next));
 
 #ifdef SPINDLE_PWM_TIMER_N
@@ -921,7 +921,7 @@ static void enumeratePins (bool low_level, pin_info_ptr pin_info)
     pin.group = PinGroup_SpindlePWM;
     pin.port = low_level ? (void *)SPINDLE_PWM_PORT : (void *)port2char(SPINDLE_PWM_PORT);
     pin.description = NULL;
-    pin_info(&pin);
+    pin_info(&pin, data);
 #endif
 }
 
@@ -1075,7 +1075,7 @@ bool driver_init (void)
     // Enable EEPROM and serial port here for Grbl to be able to configure itself and report any errors
 
     hal.info = "STM32F303";
-    hal.driver_version = "220710";
+    hal.driver_version = "220907";
 #ifdef BOARD_NAME
     hal.board = BOARD_NAME;
 #endif
@@ -1186,7 +1186,7 @@ bool driver_init (void)
 
     // No need to move version check before init.
     // Compiler will fail any signature mismatch for existing entries.
-    return hal.version == 9;
+    return hal.version == 10;
 }
 
 /* interrupt handlers */
