d90938464bae68d27b7a132f788a3f68e874aa32
Terje Io <terjeio@users.noreply.github.com>
2024-11-29 18:24:55 +0100

    Updated for core change (settings structure).

--- Src/driver.c
+++ Src/driver.c
@@ -1140,7 +1140,7 @@ static bool driver_setup (settings_t *settings)
 
 #endif
 
-    IOInitDone = settings->version == 22;
+    IOInitDone = settings->version.id == 22;
 
     hal.settings_changed(settings, (settings_changed_flags_t){0});
 
